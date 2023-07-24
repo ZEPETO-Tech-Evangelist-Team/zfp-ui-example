9abccbc23ddb1432b8f8e28bce3a1ac909a61e4a
John Lee <leehyunsik@naverz-corp.com>
2023-07-24 17:59:16 -0400

    [John] Dialogue Prefab Updates
    Added in Dynamic UI Setup. Will create scripts for dialogue system.

--- Assets/Scripts/DialogueMenuWindow.ts
+++ Assets/Scripts/DialogueMenuWindow.ts
@@ -1,10 +1,18 @@
+import { GameObject } from 'UnityEngine';
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+import DialogueOption from './DialogueOption';
 
 export default class DialogueMenuWindow extends ZepetoScriptBehaviour {
-
-    Start() {    
-
+    public dialogueGO: GameObject[] = new Array<GameObject>();
+    
+    private dialogueOptions: DialogueOption[] = new Array<DialogueOption>();
+    
+    Start() {     
+        this.dialogueGO.forEach(go => {
+            this.dialogueOptions.push(go.GetComponent<DialogueOption>());
+        })
     }
+    
 
     public ShowWindow() {
         this.gameObject.SetActive(true);
